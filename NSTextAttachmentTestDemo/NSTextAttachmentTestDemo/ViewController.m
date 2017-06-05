637139ee5e3c00eba3ec1fef6e0444bf6b7fb48d
thomas <dui1cuo@126.com>
2017-06-05 13:53:11 +0800

    [Mod]add test demo

--- NSTextAttachmentTestDemo/NSTextAttachmentTestDemo/ViewController.m
+++ NSTextAttachmentTestDemo/NSTextAttachmentTestDemo/ViewController.m
@@ -10,20 +10,36 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *testLabel;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self testNSTextAttachment];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
+
+- (void)testNSTextAttachment {
+
+    NSTextAttachment *attchment = [[NSTextAttachment alloc] init];
+    attchment.image = [UIImage imageNamed:@"test"];
+    //bounds属性能改变图片的位置。
+    attchment.bounds = CGRectMake(0, 0, 15, 15);
+    
+    //这里通过appendAttributedString 添加的先后图字的先后顺序，则是展示的前后顺序。
+    NSMutableAttributedString *attachmentString = [[NSMutableAttributedString alloc] initWithAttributedString:[NSMutableAttributedString attributedStringWithAttachment:attchment]];
+    [attachmentString appendAttributedString:[[NSAttributedString alloc] initWithString:@"测试测试测试测试测试试"]];
+    self.testLabel.attributedText = attachmentString;
+
+}
+
 
 
 @end
